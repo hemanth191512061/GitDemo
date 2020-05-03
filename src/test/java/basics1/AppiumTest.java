81e68f26c2668d8da1101d4dfb364151281cfab6
chinni <b.hemanth1997@gmail.com>
2020-05-03 08:57:32 +0530

    this is a commit from asian time zone guy

--- src/test/java/basics1/AppiumTest.java
+++ src/test/java/basics1/AppiumTest.java
@@ -6,6 +6,9 @@ public class AppiumTest {
 	@Test
 	public void appiumtest() {
 		System.out.println("this is maven selenium");
+		System.out.println("apium test 1");
+		System.out.println("apium test 4");
+		System.out.println("apium test 2");
 	}
 
 }
